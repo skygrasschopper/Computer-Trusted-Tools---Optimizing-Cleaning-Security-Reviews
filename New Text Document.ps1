3e73d7155e9445321ffbd5e04f015b8b11999e33
Skygrass Chopper <72885678+skygrasschopper@users.noreply.github.com>
2022-03-18 20:48:23 -0400

    Delete New Text Document.ps1

--- New Text Document.ps1
+++
@@ -1,6 +0,0 @@
-$acts = Get-MpPreference | Select ThreatIDDefaultAction_Actions
-Foreach ($act in $acts) {
-    if ($act.ThreatIDDefaultAction_Actions -ne $null) {
-        Remove-MpPreference -ThreatIDDefaultAction_Actions $act.ThreatIDDefaultAction_Actions
-    }
-}
