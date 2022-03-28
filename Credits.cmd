dbd01ba5c733f68f68d3a98c5c3f3e54bdf8b181
Skygrass Chopper <72885678+skygrasschopper@users.noreply.github.com>
2022-03-27 23:26:20 -0700

    Add files via upload

---
+++ Credits.cmd
@@ -0,0 +1,103 @@
+@echo off
+    MODE CON: COLS=160 LINES=51
+    color 0a
+    :credits                                                                                                                                                                                                                                                                                                           
+    echo     SSSSSSSSSSSSSSS kkkkkkkk    
+    ping 127.0.0.1 -n 1 >nul                                                                                                                           
+    echo    SS:::::::::::::::Sk::::::k    
+    ping 127.0.0.1 -n 1 >nul                                                                                                                           
+    echo    S:::::SSSSSS::::::Sk::::::k    
+    ping 127.0.0.1 -n 1 >nul                                                                                                                           
+    echo    S:::::S     SSSSSSSk::::::k 
+    ping 127.0.0.1 -n 1 >nul                                                                                                                              
+    echo     S:::::S             k:::::k    kkkkkkkyyyyyyy           yyyyyyyggggggggg   gggggrrrrr   rrrrrrrrr   aaaaaaaaaaaaa      ssssssssss       ssssssssss    
+    ping 127.0.0.1 -n 1 >nul    
+    echo    S:::::S             k:::::k   k:::::k  y:::::y         y:::::yg:::::::::ggg::::gr::::rrr:::::::::r  a::::::::::::a   ss::::::::::s    ss::::::::::s      
+    ping 127.0.0.1 -n 1 >nul 
+    echo     S::::SSSS          k:::::k  k:::::k    y:::::y       y:::::yg:::::::::::::::::gr:::::::::::::::::r aaaaaaaaa:::::ass:::::::::::::s ss:::::::::::::s   
+    ping 127.0.0.1 -n 1 >nul   
+    echo    SS::::::SSSSS     k:::::k k:::::k      y:::::y     y:::::yg::::::ggggg::::::ggrr::::::rrrrr::::::r         a::::as::::::ssss:::::ss::::::ssss:::::s
+    ping 127.0.0.1 -n 1 >nul     
+    echo         SSS::::::::SS   k::::::k:::::k        y:::::y   y:::::y g:::::g     g:::::g  r:::::r     r:::::r  aaaaaaa:::::a s:::::s  ssssss  s:::::s  ssssss  
+    ping 127.0.0.1 -n 1 >nul    
+    echo        SSSSSS::::S  k:::::::::::k          y:::::y y:::::y  g:::::g     g:::::g  r:::::r     rrrrrrraa::::::::::::a   s::::::s         s::::::s          
+    ping 127.0.0.1 -n 1 >nul 
+    echo                 S:::::S k:::::::::::k           y:::::y:::::y   g:::::g     g:::::g  r:::::r           a::::aaaa::::::a      s::::::s         s::::::s   
+    ping 127.0.0.1 -n 1 >nul     
+    echo                 S:::::S k::::::k:::::k           y:::::::::y    g::::::g    g:::::g  r:::::r          a::::a    a:::::assssss   s:::::s ssssss   s:::::s  
+    ping 127.0.0.1 -n 1 >nul    
+    echo     SSSSSSS     S:::::Sk::::::k k:::::k           y:::::::y     g:::::::ggggg:::::g  r:::::r          a::::a    a:::::as:::::ssss::::::ss:::::ssss::::::s  
+    ping 127.0.0.1 -n 1 >nul   
+    echo     S::::::SSSSSS:::::Sk::::::k  k:::::k           y:::::y       g::::::::::::::::g  r:::::r          a:::::aaaa::::::as::::::::::::::s s::::::::::::::s   
+    ping 127.0.0.1 -n 1 >nul   
+    echo     S:::::::::::::::SS k::::::k   k:::::k         y:::::y         gg::::::::::::::g  r:::::r           a::::::::::aa:::as:::::::::::ss   s:::::::::::ss   
+    ping 127.0.0.1 -n 1 >nul    
+    echo    SSSSSSSSSSSSSSS   kkkkkkkk    kkkkkkk       y:::::y            gggggggg::::::g  rrrrrrr            aaaaaaaaaa  aaaa sssssssssss      sssssssssss    
+    ping 127.0.0.1 -n 1 >nul     
+    echo                                                 y:::::y                     g:::::g                                         
+    ping 127.0.0.1 -n 1 >nul                                  
+    echo                                            y:::::y          gggggg      g:::::g                     
+    ping 127.0.0.1 -n 1 >nul                                                      
+    echo                                            y:::::y           g:::::gg   gg:::::g         
+    ping 127.0.0.1 -n 1 >nul                                                                  
+    echo                                            y:::::y             g::::::ggg:::::::g     
+    ping 127.0.0.1 -n 1 >nul                                                                      
+    echo                                            yyyyyyy               gg:::::::::::::g   
+    ping 127.0.0.1 -n 1 >nul                                                                         
+    echo                                                                     ggg::::::ggg
+    ping 127.0.0.1 -n 1 >nul                                                                           
+    echo                                                                      gggggg   
+    ping 127.0.0.1 -n 1 >nul                                                                           
+    echo.                                                                          
+    color 0b                                                                           
+    echo.                                                                     
+    ping 127.0.0.1 -n 1 >nul                                                                                   
+    echo             CCCCCCCCCCCCChhhhhhh                                    
+    ping 127.0.0.1 -n 1 >nul                                                                                          
+    echo         CCC::::::::::::Ch:::::h                   
+    ping 127.0.0.1 -n 1 >nul                                                                                                           
+    echo     CC:::::::::::::::Ch:::::h            
+    ping 127.0.0.1 -n 1 >nul                                                                                                                  
+    echo     C:::::CCCCCCCC::::Ch:::::h   
+    ping 127.0.0.1 -n 1 >nul                                                                                                                           
+    echo     C:::::C       CCCCCC h::::h hhhhh          ooooooooooo   ppppp   ppppppppp   ppppp   ppppppppp       eeeeeeeeeeee    rrrrr   rrrrrrrrr      
+    ping 127.0.0.1 -n 1 >nul             
+    echo     C:::::C               h::::hh:::::hhh     oo:::::::::::oo p::::ppp:::::::::p  p::::ppp:::::::::p    ee::::::::::::ee  r::::rrr:::::::::r
+    ping 127.0.0.1 -n 1 >nul               
+    echo     C:::::C               h::::::::::::::hh  o:::::::::::::::op:::::::::::::::::p p:::::::::::::::::p  e::::::eeeee:::::eer:::::::::::::::::r  
+    ping 127.0.0.1 -n 1 >nul               
+    echo     C:::::C               h:::::::hhh::::::h o:::::ooooo:::::opp::::::ppppp::::::ppp::::::ppppp::::::pe::::::e     e:::::err::::::rrrrr::::::r 
+    ping 127.0.0.1 -n 1 >nul               
+    echo     C:::::C               h::::::h   h::::::ho::::o     o::::o p:::::p     p:::::p p:::::p     p:::::pe:::::::eeeee::::::e r:::::r     r:::::r    
+    ping 127.0.0.1 -n 1 >nul            
+    echo     C:::::C               h:::::h     h:::::ho::::o     o::::o p:::::p     p:::::p p:::::p     p:::::pe:::::::::::::::::e  r:::::r     rrrrrrr     
+    ping 127.0.0.1 -n 1 >nul           
+    echo     C:::::C               h:::::h     h:::::ho::::o     o::::o p:::::p     p:::::p p:::::p     p:::::pe::::::eeeeeeeeeee   r:::::r         
+    ping 127.0.0.1 -n 1 >nul                   
+    echo     C:::::C       CCCCCC h:::::h     h:::::ho::::o     o::::o p:::::p    p::::::p p:::::p    p::::::pe:::::::e            r:::::r     
+    ping 127.0.0.1 -n 1 >nul                       
+    echo     C:::::CCCCCCCC::::C h:::::h     h:::::ho:::::ooooo:::::o p:::::ppppp:::::::p p:::::ppppp:::::::pe::::::::e           r:::::r    
+    ping 127.0.0.1 -n 1 >nul                        
+    echo     CC:::::::::::::::C h:::::h     h:::::ho:::::::::::::::o p::::::::::::::::p  p::::::::::::::::p  e::::::::eeeeeeee   r:::::r  
+    ping 127.0.0.1 -n 1 >nul                          
+    echo         CCC::::::::::::C h:::::h     h:::::h oo:::::::::::oo  p::::::::::::::pp   p::::::::::::::pp    ee:::::::::::::e   r:::::r   
+    ping 127.0.0.1 -n 1 >nul                         
+    echo             CCCCCCCCCCCCC hhhhhhh     hhhhhhh   ooooooooooo    p::::::pppppppp     p::::::pppppppp        eeeeeeeeeeeeee   rrrrrrr   
+    ping 127.0.0.1 -n 1 >nul                         
+    echo                                                             p:::::p             p:::::p                          
+    ping 127.0.0.1 -n 1 >nul                                          
+    echo                                                             p:::::p             p:::::p                  
+    ping 127.0.0.1 -n 1 >nul                                                  
+    echo                                                             p:::::::p           p:::::::p            
+    ping 127.0.0.1 -n 1 >nul                                                       
+    echo                                                             p:::::::p           p:::::::p       
+    ping 127.0.0.1 -n 1 >nul                                                            
+    echo                                                             p:::::::p           p:::::::p    
+    ping 127.0.0.1 -n 1 >nul                                                               
+    echo                                                             ppppppppp           ppppppppp                                                                   
+    color 0a
+    goto credits                                                                                                                                                          
+
+Thanks for using! I would also like to thank Tron and thousands of single kind heart souls that shared their projects with the world for free.
+Long live open source.
+Long live freendom.
