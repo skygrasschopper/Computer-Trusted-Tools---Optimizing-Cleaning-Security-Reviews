9ba41a7b2a61b89b513d601d5546177fdf8b823f
Skygrass Chopper <72885678+skygrasschopper@users.noreply.github.com>
2022-03-30 12:58:53 -0400

    Add files via upload

--- TOOLS (DOWNLOAD FOR REGULAR USERS)/dev_troubleshoot.cmd
+++ TOOLS (DOWNLOAD FOR REGULAR USERS)/dev_troubleshoot.cmd
@@ -1,4 +1,8 @@
-@echo off
+@echo on
+::For the batch your running, change echo off to on inside the file.
+
+::LOGFILE is where the output will be stored, in this example in C folder
 set LOGFILE=C:\batch.log
-call "C:\Users\%username%\OneDrive\USB\Tools\scripts\TOOLS (DOWNLOAD FOR REGULAR USERS)\1. This PC Optimizer [Cyber Security Editon].cmd" > %LOGFILE% 
+::Edit where the script is stored at. If it is in downloads, downloads path is C:\Users\%username%\Downloads\...\.cmd. QUOTE THE PATH WITH ""
+call "C:\Users\%username%\OneDrive\USB\Tools\scripts\TOOLS (DOWNLOAD FOR REGULAR USERS)\1. This PC Optimizer [Cyber Security Editon] - Copy.cmd" > %LOGFILE% 
 exit /B
